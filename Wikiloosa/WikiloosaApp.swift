ac509983b1c7012148b0b0e50f6d43b92884b451
Daniel von Trotha <50301900+S0ulf3re@users.noreply.github.com>
2022-01-31 13:50:35 -0500

    Initial Commit

---
+++ Wikiloosa/WikiloosaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiloosaApp.swift
+//  Wikiloosa
+//
+//  Created by Daniel von Trotha on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WikiloosaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
