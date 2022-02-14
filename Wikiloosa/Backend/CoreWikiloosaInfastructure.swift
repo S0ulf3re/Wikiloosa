67c4615afbf35b507fe314d03e1858e01c043473
Daniel von Trotha <50301900+S0ulf3re@users.noreply.github.com>
2022-02-14 14:41:28 -0500

    starting backend

---
+++ Wikiloosa/Backend/CoreWikiloosaInfastructure.swift
@@ -0,0 +1,17 @@
+//
+//  CoreWikipediaInfastructure.swift
+//  Wikiloosa
+//
+//  Created by Daniel von Trotha on 2/14/22.
+//
+
+import Foundation
+import WikipediaKit
+import SwiftUI
+
+class WikiloosaDelegate {
+    let wikipedia = Wikipedia.shared
+    func getArticle() {
+        let language = WikipediaLanguage("en")
+    }
+}
